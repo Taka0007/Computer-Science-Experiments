1a86d61b0c0d38774d677fcde7e77bdf7cde1d38
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-11 15:56:27 +0900

    Create Ex2-1.ml

---
+++ Kyoto-u/Computer Science Experiments and Exercises 3/implementation/Ex2-1.ml
@@ -0,0 +1,47 @@
+(* 入力 *)
+float_of_int 3 +. 2.5 ;;
+
+(* 出力 *)
+- : float = 5.5
+
+
+(* 入力 *)
+int_of_float 0.7 ;;
+
+(* 出力 *)
+- : int = 0
+
+
+(* 入力 *)
+if "11" > "100" then "foo" else "bar" ;;
+
+(* 出力 *)
+- : string = "foo"
+
+
+(* 入力 *)
+if "11" > "100" then "foo" else "bar" ;;
+
+(* 出力 *)
+- : string = "foo"
+
+
+(* 入力 *)
+char_of_int ((int_of_char 'A') + 20) ;;
+
+(* 出力 *)
+- : char = 'U'
+
+
+(* 入力 *)
+int_of_string "0xff" ;;
+
+(* 出力 *)
+- : int = 255
+
+
+(* 入力 *)
+5.0 ** 2.0 ;;
+
+(* 出力 *)
+- : float = 25.
