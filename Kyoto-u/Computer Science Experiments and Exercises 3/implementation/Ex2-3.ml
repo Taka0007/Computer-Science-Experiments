d98a62e7616d8923b97ae089f306faadade9c35d
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-12 13:52:16 +0900

    Update Ex2-3.ml

--- Kyoto-u/Computer Science Experiments and Exercises 3/implementation/Ex2-3.ml
+++ Kyoto-u/Computer Science Experiments and Exercises 3/implementation/Ex2-3.ml
@@ -5,11 +5,33 @@ not (true && false) ;;
 float_of_int(int_of_float(5.0))
 
 
-
+(* 3 *)
+float_of_int(int_of_float (sin (3.14 /. 2.0 ** 2.0) +. cos (3.14 /. 2.0 ** 2.0)))
 
 
 (* 4 *)
 int_of_float(sqrt(float_of_int(3*3 + 4*4)))
 
 
-(* 一旦飛ばす *)
+
+(* 以下、出力結果 *)
+
+(* 1 *)
+not (true && false) ;;
+- : bool = true
+
+
+(* 2 *)
+float_of_int(int_of_float(5.0)) ;;
+- : float = 5.
+
+
+
+(* 3 *)
+float_of_int(int_of_float (sin (3.14 /. 2.0 ** 2.0) +. cos (3.14 /. 2.0 ** 2.0))) ;;
+- : float = 1.
+
+
+(* 4 *)
+int_of_float(sqrt(float_of_int(3*3 + 4*4))) ;;
+- : int = 5
