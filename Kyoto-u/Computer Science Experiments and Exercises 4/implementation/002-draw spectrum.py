29ea3010a67627be73dcba1f80c95ad92517d6fd
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-25 11:16:36 +0900

    Update 002-draw spectrum.py

--- Kyoto-u/Computer Science Experiments and Exercises 4/implementation/002-draw spectrum.py
+++ Kyoto-u/Computer Science Experiments and Exercises 4/implementation/002-draw spectrum.py
@@ -18,8 +18,8 @@ fft_log_abs_spec = np.log(np.abs(fft_spec))
 
 #スペクトルを描画
 fig = plt.figure()
-plt.xlabel('frequency [Hz]') # x 軸 の ラ ベ ル を 設 定
-plt.ylabel('amplitude') # y 軸 の ラ ベ ル を 設 定
+plt.xlabel('frequency [Hz]') # x軸のラベルを設定
+plt.ylabel('amplitude') # y軸のラベルを設定
 plt.xlim([0, SR/2]) 
 x_data = np.linspace((SR/2)/len(fft_log_abs_spec), SR/2, len(fft_log_abs_spec))
 plt.plot(x_data, fft_log_abs_spec)
@@ -28,12 +28,12 @@ plt.show()
 
 
 
-# 横軸を0-2000Hzに拡大
+# 横 軸 を0 ̃2000 H zに拡大
 # xlimで表示の領域を変えるだけ
 fig = plt.figure()
 plt.xlabel('frequency [Hz]')
 plt.ylabel('amplitude')
 plt.xlim([0, 2000])
 plt.plot(x_data, fft_log_abs_spec)
-#表示
+# 表 示
 plt.show()
