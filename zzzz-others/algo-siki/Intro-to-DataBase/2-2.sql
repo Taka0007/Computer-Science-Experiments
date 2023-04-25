e510bbf32fe86383edf375869961c69ddc007426
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-25 22:29:08 +0900

    Create 2-2.sql

---
+++ zzzz-others/algo-siki/Intro-to-DataBase/2-2.sql
@@ -0,0 +1,13 @@
+-- 地方ごとに集計(グループ分け)し、地方ごとの総面積を大きい順に表示
+
+SELECT
+    region AS '地方名' , 
+    COUNT(*) AS '都道府県数', 
+    SUM(area) AS '総面積' 
+
+FROM
+    prefectures  GROUP BY region
+
+ORDER BY
+    SUM(area)
+    DESC;
