a9d8d3de1e2f90af5d87aab9aaa2918171065fd7
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-28 23:33:02 +0900

    Create 3-7.py

---
+++ zzzz-others/algo-siki/Intro-to-Python/3-7.py
@@ -0,0 +1,8 @@
+# 1 辺の長さが num の正方形の面積を計算
+# num: int 型
+# 返り値: int 型
+def square_area(num):
+    return num*num
+
+# 指定された値を出力してください
+print(square_area(10), square_area(123), square_area(3141) , sep='\n')
