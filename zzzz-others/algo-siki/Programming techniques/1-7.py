24df61d1c24ce8f397997f72af319af3e3112d36
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-01 01:16:21 +0900

    Create 1-7.py

---
+++ zzzz-others/algo-siki/Programming techniques/1-7.py
@@ -0,0 +1,26 @@
+# 1番目の人の偏差値を算出
+# 操作を細分化することを意識
+
+# 感想
+# 結構実装難易度高め？？
+
+def mean(li,N):
+    ans = sum(li)/N
+    return ans
+
+def st(li,me,N):
+    pre = 0
+    for i in li:
+        pre += ((i-me)**2) /N
+    ans = pre ** (1/2)
+    return ans
+
+# 生徒の人数
+N = 5
+# 試験の点数
+scores = [70, 50, 85, 69, 63]
+
+score_mean = mean(scores,N)
+score_std  = st(scores,score_mean,N)
+ans = 50 + ((scores[0]-score_mean)/score_std)*10
+print(ans)
