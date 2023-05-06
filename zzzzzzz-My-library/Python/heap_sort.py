652ddb0250c89e5ddcba823465de7317090e0974
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-06 15:38:56 +0900

    Create heap_sort.py

---
+++ zzzzzzz-My-library/Python/heap_sort.py
@@ -0,0 +1,15 @@
+import heapq
+import random
+
+def heap_sort(array):
+    
+    heap = []
+    
+    for v in array:
+        heapq.heappush(heap,v)
+    
+    return [heapq.heappop(heap) for i in range(len(heap))]
+    
+
+array = [random.randint(0,6000) for i in range(6000)]
+print(heap_sort(array))
